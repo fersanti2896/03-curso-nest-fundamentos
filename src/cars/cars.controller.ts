d3e97532fb18ac7ac50e40b10de2872896730caf
Fernando Nicolás <fersanti2896@gmail.com>
2023-08-22 16:19:17 -0600

    Creando controlador y servicio de cars.

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
+    constructor( 
+        private readonly carsService: CarsService
+    ) {}
 
     @Get()
     getAllCars() {
-        return ['Toyota', 'KIA', 'Volkswagen']
+        return this.carsService.findAll();
+    }
+
+    @Get(':id')
+    getCarById( @Param('id') id ) {
+        return this.carsService.findById( id );
     }
 }
---
+++ src/cars/cars.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+    private cars = [
+        {
+            id: 1,
+            brand: 'Toyota',
+            model: 'Corolla'
+        },
+        {
+            id: 2,
+            brand: 'Honda',
+            model: 'Civic'
+        },
+        {
+            id: 3,
+            brand: 'Jeep',
+            model: 'Cherokee'  
+        }
+    ];
+
+    findAll() {
+        return this.cars;
+    }   
+
+    findById( id: Number ) {
+        const car = this.cars.find( car => car.id == id  );
+
+        if( !car ) {
+            return {
+                msg: `No existe el carro con el id: ${ id }.`
+            }
+        }
+        return {
+            car
+        }
+    }
+}
